5a91374ce917914a2a207d6f8f63bfc60f956a80
Hayun218 <21800325@handong.edu>
2019-04-06 23:07:06 +0900

    remove save_file function

--- user.h
+++ user.h
@@ -17,7 +17,9 @@ typedef struct login {
 } LOGIN;
 
 int load_file(LOGIN* list[], char* filename);
-void join(LOGIN* list[], int count);
+int join(LOGIN* list[], int count, char* filename);
 int login(LOGIN* list[], int count);
 void logout(int* is_login);
-void save_file(LOGIN* list[], int count, char* filename);
+//void save_file(LOGIN* list[], int count, char* filename);
+void print_list(LOGIN* list[], char* filename);
+
