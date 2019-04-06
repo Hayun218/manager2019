a38a60cfacc9cc74a4eaff25e1758adcebb56b1c
Hayun218 <21800325@handong.edu>
2019-04-07 02:16:13 +0900

    counting the added records

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
   LOGIN* userlist[100]; // 사용자목록 포인터 배열 (최대 100)
   int is_login=0; // 로그인 여부 (0 NO, 1 Yes)
-  int menu_id, choice;
+  int choice;
   if (argc != 2) {
     printf("Usage : manager <datafile>\n");
     return 0;
@@ -20,18 +20,40 @@ int main(int argc, char* argv[]) {
     }
     else return 0;
   }
-
+    printf("> Welcome!!\n");
   int count = load_file(userlist, argv[1]);
+  int new = 0;
+  char *menu_id;
   while(1){
-    if(is_login==0) is_login = login(userlist, count);
-    menu_id = ask_menu(is_login); // 현재 로그인 되었는지의 여부를 파라미터로 알려야 한다.
-    if(menu_id==1) count = join(userlist, count, argv[1]);
-    else if (menu_id==2) is_login = login(userlist, count);
-    else if (menu_id==3) print_list(userlist, argv[1]);
-    else if (menu_id==4) logout(&is_login);
-    else break;
+    if(is_login==0 || is_login==1) menu_id = ask_menu(is_login); // 현재 로그인 되었는지의 여부를 파라미터로 알려야 한다.
+    if(is_login ==1){
+      if(!(strcmp(menu_id, "logout") == 0)){
+        char word[100];
+        fgets(word, 100, stdin);
+        printf("%s%s", menu_id, word);
+        continue;
+      }
+    }
+    if(strcmp(menu_id, "join") == 0){
+      new = join(userlist, count, argv[1], new);
+      if(new >= 1) count = count+new;
+    }
+    else if (strcmp(menu_id, "login") == 0) is_login = login(userlist, count);
+    else if (strcmp(menu_id, "list") == 0) print_list(userlist, argv[1]);
+    else if (strcmp(menu_id, "logout") == 0) {
+      if(is_login == 0) {
+        printf("No such command!\n");
+      }
+      else  {
+        printf("Bye!!\n");
+        logout(&is_login);
+      }
+    }
+    else if(strcmp(menu_id, "exit") == 0 ) {
+      printf("%d records saved to %s\n", new, argv[1]);
+      return 0;
+    }
+    else printf("No such command\n");
   }
-  //save_file(userlist, count, argv[1]); => join에 바로 구현함으로 해당 함수는 삭제함 
-  return 0;
 }
 
